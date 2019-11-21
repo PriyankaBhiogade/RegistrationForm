b425e7afe54b6bd51e88068222223f1019c7af21
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 16:49:07 +0530

    Add:functionality to check at least 1Upper case

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -3,10 +3,11 @@ echo "User Registration Form"
 checkFirstLast="^[A-Z][a-z]*{2,}$"
 checkEmail="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
 checkMobileNum="^[0-9]{2}[[:space:]][0-9]{10}$"
-checkPassword="^[a-zA-Z]{8,}$"
+checkPassword="[a-zA-Z]*[A-Z]+[a-zA-Z]*$"
+
 function checkFirstName(){
 	read -p "Enter First Name :: " firstName 
-	if [[ $firstName =~ $checkFirstLast ]];
+	if [[ $firstName =~ $checkFirstLast ]] 
  	then
         	echo "First Name is Valid" ;
 	else
@@ -47,18 +48,20 @@ function checkMobileNum(){
 }
 
 function checkPassword(){
-	read -p "Enter your password :: " password
-	if [[ $password =~ $checkPassword ]];
-	then
-		echo "Password is valid"
-	else
-		echo "Password is Invalid"
-	fi
+        read -p "Enter your password :: " password
+        if [[ $password =~ $checkPassword ]] 
+        then
+                echo "Password is valid"
+        else
+                echo "Password is Invalid"
+        fi
 }
 
 
-checkFirstName
-checkLastName
-checkEmail
-checkMobileNum
+
+
+#checkFirstName
+#checkLastName
+#checkEmail
+#checkMobileNum
 checkPassword
