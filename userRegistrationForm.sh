2d582d090276eb3532698471795cf34facc17477
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 10:28:35 +0530

    Add : function to check lastName

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 echo "User Registration Form"
 read -p "Enter First Name :: " firstName
+read -p "Enter Last Name :: " lastName
 checkPattern="^[A-Z]+[a-zA-Z]{2,}"
 function CheckFirstName(){
 	if [[ $firstName =~ $checkPattern ]];
@@ -10,4 +11,14 @@ function CheckFirstName(){
         	echo "First Name is Invalid";
 	fi
 }
+
+function CheckLastName(){
+        if [[ $lastName =~ $checkPattern ]];
+        then
+                echo "Last Name is Valid" ;
+        else
+                echo "Last Name is Invalid";
+        fi
+}
 CheckFirstName
+CheckLastName
