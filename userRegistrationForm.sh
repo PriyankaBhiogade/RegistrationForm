057232cd696a1b8b20eb7576baceac7b9c2c84e2
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 10:18:40 +0530

    Refactor:put code in function

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -2,9 +2,12 @@
 echo "User Registration Form"
 read -p "Enter First Name :: " firstName
 checkPattern="^[A-Z]+[a-zA-Z]{2,}"
-if [[ $firstName =~ $checkPattern ]];
-then
-        echo "First Name is Valid" ;
-else
-        echo "First Name is Invalid";
-fi
+function CheckFirstName(){
+	if [[ $firstName =~ $checkPattern ]];
+	then
+        	echo "First Name is Valid" ;
+	else
+        	echo "First Name is Invalid";
+	fi
+}
+CheckFirstName
