2063e6775855ba9084ab1d9a93e96b306636f5a6
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 10:11:58 +0530

    Add : check valid First name

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "User Registration Form"
-
+read -p "Enter First Name :: " firstName
+checkPattern="^[A-Z]+[a-zA-Z]{2,}"
+if [[ $firstName =~ $checkPattern ]];
+then
+        echo "First Name is Valid" ;
+else
+        echo "First Name is Invalid";
+fi
