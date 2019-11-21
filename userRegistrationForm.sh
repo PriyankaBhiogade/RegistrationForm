20f5f7a6310dceaed0d0792529fe2b30871feebe
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 10:43:21 +0530

    Add:function to check valid email

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 echo "User Registration Form"
-read -p "Enter First Name :: " firstName
-read -p "Enter Last Name :: " lastName
-checkPattern="^[A-Z]+[a-zA-Z]{2,}"
-function CheckFirstName(){
-	if [[ $firstName =~ $checkPattern ]];
+checkFirstLast="^[A-Z]+[a-zA-Z]{2,}"
+checkEmail="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
+
+function checkFirstName(){
+	read -p "Enter First Name :: " firstName
+	if [[ $firstName =~ $checkFirstLast ]];
 	then
         	echo "First Name is Valid" ;
 	else
@@ -12,13 +13,27 @@ function CheckFirstName(){
 	fi
 }
 
-function CheckLastName(){
-        if [[ $lastName =~ $checkPattern ]];
+function checkLastName(){
+	read -p "Enter Last Name :: " lastName
+        if [[ $lastName =~ $checkFirstLast ]];
         then
                 echo "Last Name is Valid" ;
         else
                 echo "Last Name is Invalid";
         fi
 }
-CheckFirstName
-CheckLastName
+
+function checkEmail(){
+	 read -p "Enter your Email:: " email
+        if [[ $email =~ $checkEmail ]];
+        then
+                echo "Email is Valid" ;
+        else
+                echo "Email is Invalid";
+        fi
+
+}
+
+checkFirstName
+checkLastName
+checkEmail
