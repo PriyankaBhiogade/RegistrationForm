cc96fdd4b91e4a5b5a4754b21d2b2e06fa184d69
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 17:59:51 +0530

    Add:functionality to at least check 1 number

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -3,7 +3,9 @@ echo "User Registration Form"
 checkFirstLast="^[A-Z][a-z]*{2,}$"
 checkEmail="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
 checkMobileNum="^[0-9]{2}[[:space:]][0-9]{10}$"
-checkPassword="[a-zA-Z]*[A-Z]+[a-zA-Z]*$"
+checkPassword1="^[a-zA-Z]{8,}$"
+checkPassword2="[a-zA-Z]*[A-Z]+[a-zA-Z]*$"
+checkPassword3="^(.*+[0-9]+.*[A-Z]+[a-zA-Z0-9])"
 
 function checkFirstName(){
 	read -p "Enter First Name :: " firstName 
@@ -47,9 +49,29 @@ function checkMobileNum(){
 
 }
 
-function checkPassword(){
+function checkPassword1(){
         read -p "Enter your password :: " password
-        if [[ $password =~ $checkPassword ]] 
+        if [[ $password =~ $checkPassword1 ]] 
+        then
+                echo "Password is valid"
+        else
+                echo "Password is Invalid"
+        fi
+}
+
+function checkPassword2(){
+        read -p "Enter your password :: " password
+        if [[ $password =~ $checkPassword2 ]] 
+        then
+                echo "Password is valid"
+        else
+                echo "Password is Invalid"
+        fi
+}
+
+function checkPassword3(){
+        read -p "Enter your password :: " password
+        if [[ $password =~ $checkPassword3 ]] 
         then
                 echo "Password is valid"
         else
@@ -60,8 +82,10 @@ function checkPassword(){
 
 
 
-#checkFirstName
-#checkLastName
-#checkEmail
-#checkMobileNum
-checkPassword
+checkFirstName
+checkLastName
+checkEmail
+checkMobileNum
+checkPassword1
+checkPassword2
+checkPassword3
