7078eebc4dc9f1931471aaf076b685b867770a04
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 11:25:59 +0530

    Add: Functionality to check valid mobile num

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 echo "User Registration Form"
-checkFirstLast="^[A-Z]+[a-zA-Z]{2,}"
+checkFirstLast="^[A-Z][a-z]*{2,}$"
 checkEmail="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
+checkMobileNum="^[0-9]{2}[[:space:]][0-9]{10}$"
 
 function checkFirstName(){
-	read -p "Enter First Name :: " firstName
+	read -p "Enter First Name :: " firstName 
 	if [[ $firstName =~ $checkFirstLast ]];
-	then
+ 	then
         	echo "First Name is Valid" ;
 	else
         	echo "First Name is Invalid";
@@ -14,7 +15,7 @@ function checkFirstName(){
 }
 
 function checkLastName(){
-	read -p "Enter Last Name :: " lastName
+	read -p "Enter Last Name :: " lastName 
         if [[ $lastName =~ $checkFirstLast ]];
         then
                 echo "Last Name is Valid" ;
@@ -24,7 +25,7 @@ function checkLastName(){
 }
 
 function checkEmail(){
-	 read -p "Enter your Email:: " email
+	 read -p "Enter your Email:: " email 
         if [[ $email =~ $checkEmail ]];
         then
                 echo "Email is Valid" ;
@@ -34,6 +35,19 @@ function checkEmail(){
 
 }
 
+function checkMobileNum(){
+        read -p "Enter your Mobile Number:: " mobileNum 
+        if [[ $mobileNum =~ $checkMobileNum ]];
+        then
+                echo "Mobile is Valid" ;
+        else
+                echo "Email is Invalid";
+        fi
+
+}
+
+
 checkFirstName
 checkLastName
 checkEmail
+checkMobileNum
