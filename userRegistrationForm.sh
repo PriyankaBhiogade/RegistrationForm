b577672274ad43ed75ac40284c3a795a9d50173a
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-21 11:48:07 +0530

    Add :functionality to check min 8 characters

--- userRegistrationForm.sh
+++ userRegistrationForm.sh
@@ -3,7 +3,7 @@ echo "User Registration Form"
 checkFirstLast="^[A-Z][a-z]*{2,}$"
 checkEmail="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
 checkMobileNum="^[0-9]{2}[[:space:]][0-9]{10}$"
-
+checkPassword="^[a-zA-Z]{8,}$"
 function checkFirstName(){
 	read -p "Enter First Name :: " firstName 
 	if [[ $firstName =~ $checkFirstLast ]];
@@ -46,8 +46,19 @@ function checkMobileNum(){
 
 }
 
+function checkPassword(){
+	read -p "Enter your password :: " password
+	if [[ $password =~ $checkPassword ]];
+	then
+		echo "Password is valid"
+	else
+		echo "Password is Invalid"
+	fi
+}
+
 
 checkFirstName
 checkLastName
 checkEmail
 checkMobileNum
+checkPassword
